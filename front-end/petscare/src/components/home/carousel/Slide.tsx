f060417490243b4334319c901d732e7a3831dc73
Marina Victoria Blanco <marivic_05@hotmail.com>
2023-10-27 20:28:10 -0300

    Slider

--- front-end/petscare/src/components/home/carousel/Slide.tsx
+++ front-end/petscare/src/components/home/carousel/Slide.tsx
@@ -2,20 +2,23 @@ import Paper from '@mui/material/Paper'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import styles from './Slide.module.css'
+import { StaticImageData } from 'next/image'
+
 
 interface Props {
     item: {
         title: string;
         description: string;
+       image: StaticImageData
     }
 }
 
 const Slide = ({ item }: Props) => {
     return (
-        <Paper>
-            <Container>
+        <Paper className={styles.paper}>
+            <Container className={styles.container} >
                 <Typography className={styles.title}>{item.title}</Typography>
-                <Typography className={styles.title}>{item.description}</Typography>
+                <Typography className={styles.description}>{item.description}</Typography>
             </Container>
         </Paper>
     )
