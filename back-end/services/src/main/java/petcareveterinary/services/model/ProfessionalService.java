9fffcc62aa0bfb734f44bf02f5144c85a30f546a
JulianaAparicio <juliana_aparicio@outlook.com>
2023-11-03 19:15:24 -0300

    Fixed getter and setter error

--- back-end/services/src/main/java/petcareveterinary/services/model/ProfessionalService.java
+++ back-end/services/src/main/java/petcareveterinary/services/model/ProfessionalService.java
@@ -1,14 +1,13 @@
 package petcareveterinary.services.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
-@Data
+
 @Entity
-@Table(name="Services")
+@Table(name = "Services")
 public class ProfessionalService {
     @Id
-    @SequenceGenerator(name="service_sequence", sequenceName = "service_sequence")
+    @SequenceGenerator(name = "service_sequence", sequenceName = "service_sequence")
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "service_sequence")
     private Long Id;
     @Column
@@ -17,4 +16,48 @@ public class ProfessionalService {
     private String description;
     @Column
     private String thumbnail;
+
+    public ProfessionalService(Long id, String name, String description, String thumbnail) {
+        Id = id;
+        this.name = name;
+        this.description = description;
+        this.thumbnail = thumbnail;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
 }
+
+
+
+
+
