f5fbf2a70bd3a52b4e62a8211d35ec9c6550cff4
JulianaAparicio <juliana_aparicio@outlook.com>
2023-11-03 18:47:26 -0300

    Added service interface

--- back-end/services/src/main/java/petcareveterinary/services/service/IProfessionalService.java
+++ back-end/services/src/main/java/petcareveterinary/services/service/IProfessionalService.java
@@ -1,4 +1,18 @@
 package petcareveterinary.services.service;
 
+import petcareveterinary.services.model.ProfessionalService;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IProfessionalService {
+    List<ProfessionalService> listProfessionalServices();
+
+    Optional<ProfessionalService> searchProfessionalServiceById(Long id);
+
+    ProfessionalService createProfessionalService(ProfessionalService professional);
+
+    void deleteProfessionalService(Long id) throws Exception;
+
+    ProfessionalService updateProfessionalService(ProfessionalService professional) throws Exception;
 }
