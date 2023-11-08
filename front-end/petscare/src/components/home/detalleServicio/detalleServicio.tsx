ece01066fdc573f26df03c7399a4a2d9a37bde56
Marina Victoria Blanco <marivic_05@hotmail.com>
2023-11-08 20:42:14 -0300

    Ajuste tipo dato en Detalle Servicio

--- front-end/petscare/src/components/home/detalleServicio/detalleServicio.tsx
+++ front-end/petscare/src/components/home/detalleServicio/detalleServicio.tsx
@@ -1,59 +1,59 @@
 import { Serv } from "@/interfaces/servicios";
 import styles from "./Detalle.module.css";
-import {Container, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Typography } from "@mui/material";
+import { Container, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Typography } from "@mui/material";
 import { FC, useState } from "react";
 import Image from 'next/image'
-import {profData} from '../servicios/profData'
+import { profData } from '../../professionals/profData'
 
 interface Props {
-    servicio : Serv,
+    servicio: Serv,
     visible: string
 }
 
 
-export const DetalleServ: FC<Props> = ({servicio, visible}) => {
-   
-    const [profName, setprofName] = useState<string[]>([]);
+const DetalleServ: FC<Props> = ({ servicio, visible }) => {
+
+    const [profName, setprofName] = useState<string>('');
     const profesionales = profData
-    
+
     const handleChange = (event: SelectChangeEvent<typeof profName>) => {
         setprofName(event.target.value);
     };
-          
-    return(       
-        <Container className={styles.wrapperDet} style={{ display : `${visible}`}}>            
-                <Image className={styles.imgDet} 
-                    src={servicio?.url}                    
-                    alt={"cirugia"}
-                    width= {340}
-                    height = {280}
-                />
-                <Typography className={styles.titleDet}>
-                    
-                    {servicio?.nombre}
-                </Typography>
-                <Typography >
-                 {servicio?.descripcion}
-                </Typography>
-                <FormControl variant="standard"  sx={{ m: 2, width: 300 }}>
-                    <InputLabel id="demo-multiple-name-label">LIstado de Profesionales</InputLabel>
-                    <Select
-                    labelId="demo-simple-select-standard-label"
-                    id="demo-simple-select-standard"
+
+    return (
+        <Container className={styles.wrapperDet} style={{ display: `${visible}` }}>
+            <Image className={styles.imgDet}
+                src={servicio?.url}
+                alt={"cirugia"}
+                width={340}
+                height={280}
+            />
+            <Typography className={styles.titleDet}>
+
+                {servicio?.nombre}
+            </Typography>
+            <Typography >
+                {servicio?.descripcion}
+            </Typography>
+            <FormControl variant="standard" sx={{ m: 2, width: 300 }}>
+                <InputLabel>LIstado de Profesionales</InputLabel>
+                <Select
                     value={profName}
                     onChange={handleChange}
-                    >
+                >
                     {profesionales?.map((profesional) => (
                         <MenuItem
-                        key={profesional.id}
-                        value={profesional.nombre}
+                            key={profesional.id}
+                            value={profesional.nombre}
                         >
-                        {profesional.nombre}
+                            {profesional.nombre}
                         </MenuItem>
                     ))}
-                    </Select>
-                </FormControl>           
+                </Select>
+            </FormControl>
         </Container>
     )
 
 }
+
+export default DetalleServ
