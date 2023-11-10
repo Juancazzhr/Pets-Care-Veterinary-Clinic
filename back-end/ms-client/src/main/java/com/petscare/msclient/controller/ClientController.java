b3a3e90db179f7644eb583f736416c17782534f9
rfescobar <ferchuescobar@gmail.com>
2023-11-10 01:19:32 -0300

    Endpoint documentation for users management (Swagger)

--- back-end/ms-client/src/main/java/com/petscare/msclient/controller/ClientController.java
+++ back-end/ms-client/src/main/java/com/petscare/msclient/controller/ClientController.java
@@ -2,10 +2,15 @@ package com.petscare.msclient.controller;
 
 import com.petscare.msclient.model.Client;
 import com.petscare.msclient.service.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 
 import java.util.List;
 
@@ -13,52 +18,76 @@ import java.util.List;
 @RequestMapping("/v1/clients")
 public class ClientController {
 
-
     ClientService clientService;
 
-    public ClientController(ClientService clientService) {this.clientService = clientService;}
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
 
+    @Operation(summary = "Create a new client")
     @PostMapping
-    public ResponseEntity<String> createClient(@RequestBody Client client){
+    public ResponseEntity<String> createClient(@RequestBody Client client) {
         clientService.createClient(client);
         return ResponseEntity.status(HttpStatus.CREATED).body("Cliente creado correctamente");
     }
 
+    @Operation(summary = "Get all clients")
     @GetMapping
     public List<Client> getAll() {
         return clientService.getAll();
     }
 
-
-
+    @Operation(summary = "Get a client by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the client",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Client.class)) }),
+            @ApiResponse(responseCode = "404", description = "Client not found",
+                    content = @Content)
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<Client> searchClientById(@PathVariable Long id){
-        if(clientService.getClientById(id).isPresent()){
+    public ResponseEntity<Client> searchClientById(@Parameter(description = "ID of the client to be searched")
+                                                   @PathVariable Long id) {
+        if (clientService.getClientById(id).isPresent()) {
             return ResponseEntity.ok(clientService.getClientById(id).get());
-        }else{
+        } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 
+    @Operation(summary = "Update an existing client")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated the client",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Client.class)) }),
+            @ApiResponse(responseCode = "404", description = "Client not found",
+                    content = @Content)
+    })
     @PutMapping
-    public  ResponseEntity<Client> updateClient(@RequestBody Client client) throws Exception {
-        if(clientService.getClientById(client.getId()).isPresent()){
+    public ResponseEntity<Client> updateClient(@RequestBody Client client) throws Exception {
+        if (clientService.getClientById(client.getId()).isPresent()) {
             return ResponseEntity.ok(clientService.updateClient(client));
-        }else{
-            ResponseEntity.status(HttpStatus.NOT_FOUND).body("El cliente con el id "+ client.getId()+ " no existe");
+        } else {
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body("El cliente con el id " + client.getId() + " no existe");
             throw new Exception();
         }
     }
 
+    @Operation(summary = "Delete a client by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Client deleted"),
+            @ApiResponse(responseCode = "404", description = "Client not found",
+                    content = @Content)
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteClient(@PathVariable Long id) throws Exception {
-        if (clientService.getClientById(id).isPresent()){
+    public ResponseEntity<String> deleteClient(@Parameter(description = "ID of the client to be deleted")
+                                               @PathVariable Long id) throws Exception {
+        if (clientService.getClientById(id).isPresent()) {
             clientService.deleteClient(id);
             return ResponseEntity.ok("El cliente fue eliminado correctamente");
-        }else{
-            ResponseEntity.status(HttpStatus.NOT_FOUND).body("El cliente con el id "+id+" no existe");
+        } else {
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body("El cliente con el id " + id + " no existe");
             throw new Exception();
         }
     }
-
 }
