2f7c43583af4a13e25dda760d00b5b4e4eb2aa69
Marina Victoria Blanco <marivic_05@hotmail.com>
2023-11-13 17:28:05 -0300

    Arreglo turnero

--- front-end/petscare/src/components/appointments/Appointments.tsx
+++ front-end/petscare/src/components/appointments/Appointments.tsx
@@ -1,11 +1,6 @@
 import { FC } from 'react'
 import Paper from '@mui/material/Paper'
-import Box from '@mui/material/Box'
 import styles from './Appointments.module.css'
-
-import PetSelect from './formItems/PetSelect'
-import AddCircleIcon from '@mui/icons-material/AddCircle'
-import Typography from '@mui/material/Typography'
 import StepperAppointment from './formItems/stepper/StepperAppointment'
 
 const Appointments: FC = () => {
