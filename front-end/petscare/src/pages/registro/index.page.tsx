aef2b4c4a03e279cba8c740a424f9edd2d9475b1
Marina Victoria Blanco <marivic_05@hotmail.com>
2023-11-13 18:24:51 -0300

    Ajustes css layoutAuth, login y registro

--- front-end/petscare/src/pages/registro/index.page.tsx
+++ front-end/petscare/src/pages/registro/index.page.tsx
@@ -19,6 +19,7 @@ import {
   Typography,
   Paper,
   Link,
+  Stack,
 } from "@mui/material";
 
 const Register: NextPage = () => {
@@ -59,7 +60,7 @@ const Register: NextPage = () => {
   };
 
   console.log(formik.errors);
-  
+
 
   return (
     <Box className={styles.root}>
@@ -205,7 +206,7 @@ const Register: NextPage = () => {
                 }
                 helperText={
                   formik.touched.user?.confirmPassword &&
-                  formik.errors.user?.confirmPassword
+                    formik.errors.user?.confirmPassword
                     ? formik.errors.user?.confirmPassword
                     : ""
                 }
@@ -216,100 +217,105 @@ const Register: NextPage = () => {
                 Tu Mascota
               </Typography>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                id="petName"
-                name="petName"
-                label="Nombre"
-                value={formik.values.pet.petName}
-                onChange={(event) =>
-                  formik.setFieldValue("pet.petName", event.target.value)
-                }
-                error={
-                  formik.touched.pet?.petName &&
-                  Boolean(formik.errors.pet?.petName)
-                }
-                helperText={
-                  formik.touched.pet?.petName && formik.errors.pet?.petName
-                }
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <FormControl
-                fullWidth
-                error={
-                  formik.touched.pet?.type && Boolean(formik.errors.pet?.type)
-                }
-              >
-                <InputLabel id="mascota-tipo-label">Seleccioná Tipo</InputLabel>
-                <Select
-                  labelId="mascota-tipo-label"
-                  id="type"
-                  name="type"
-                  value={formik.values.pet.type}
-                  onChange={(event) =>
-                    formik.setFieldValue("pet.type", event.target.value)
-                  }
-                >
-                  <MenuItem value="tipo1">perro</MenuItem>
-                  <MenuItem value="tipo2">gato</MenuItem>
-                  <MenuItem value="tipo3">conejo</MenuItem>
-                  <MenuItem value="tipo4">tortuga</MenuItem>
-                  <MenuItem value="tipo5">loro</MenuItem>
-                  <MenuItem value="tipo6">hámster</MenuItem>
-                  <MenuItem value="tipo7">otro</MenuItem>
-                </Select>
-                <FormHelperText>
-                  {formik.touched.pet?.type && formik.errors.pet?.type}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                id="race"
-                name="race"
-                label="Raza"
-                value={formik.values.pet.race}
-                onChange={(event) =>
-                  formik.setFieldValue("pet.race", event.target.value)
-                }
-                error={
-                  formik.touched.pet?.race && Boolean(formik.errors.pet?.race)
-                }
-                helperText={formik.touched.pet?.race && formik.errors.pet?.race}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <FormControl
-                fullWidth
-                error={
-                  formik.touched.pet?.size && Boolean(formik.errors.pet?.size)
-                }
-              >
-                <InputLabel id="mascota-tamano-label">
-                  Seleccioná Tamaño
-                </InputLabel>
-                <Select
-                  labelId="mascota-tamano-label"
-                  id="size"
-                  name="size"
-                  value={formik.values.pet.size}
-                  onChange={(event) =>
-                    formik.setFieldValue("pet.size", event.target.value)
-                  }
-                >
-                  <MenuItem value="tipo1">pequeño</MenuItem>
-                  <MenuItem value="tipo2">mediano</MenuItem>
-                  <MenuItem value="tipo3">grande</MenuItem>
-                </Select>
-                <FormHelperText>
-                  {formik.touched.pet?.size && formik.errors.pet?.size}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
+            <Stack className={styles.bgBoxMascota}>
+              <Grid container spacing={2} >
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    fullWidth
+                    id="petName"
+                    name="petName"
+                    label="Nombre"
+                    value={formik.values.pet.petName}
+                    onChange={(event) =>
+                      formik.setFieldValue("pet.petName", event.target.value)
+                    }
+                    error={
+                      formik.touched.pet?.petName &&
+                      Boolean(formik.errors.pet?.petName)
+                    }
+                    helperText={
+                      formik.touched.pet?.petName && formik.errors.pet?.petName
+                    }
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <FormControl
+                    fullWidth
+                    error={
+                      formik.touched.pet?.type && Boolean(formik.errors.pet?.type)
+                    }
+                  >
+                    <InputLabel id="mascota-tipo-label">Seleccioná Tipo</InputLabel>
+                    <Select
+                      labelId="mascota-tipo-label"
+                      id="type"
+                      name="type"
+                      value={formik.values.pet.type}
+                      onChange={(event) =>
+                        formik.setFieldValue("pet.type", event.target.value)
+                      }
+                    >
+                      <MenuItem value="tipo1">perro</MenuItem>
+                      <MenuItem value="tipo2">gato</MenuItem>
+                      <MenuItem value="tipo3">conejo</MenuItem>
+                      <MenuItem value="tipo4">tortuga</MenuItem>
+                      <MenuItem value="tipo5">loro</MenuItem>
+                      <MenuItem value="tipo6">hámster</MenuItem>
+                      <MenuItem value="tipo7">otro</MenuItem>
+                    </Select>
+                    <FormHelperText>
+                      {formik.touched.pet?.type && formik.errors.pet?.type}
+                    </FormHelperText>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    fullWidth
+                    id="race"
+                    name="race"
+                    label="Raza"
+                    value={formik.values.pet.race}
+                    onChange={(event) =>
+                      formik.setFieldValue("pet.race", event.target.value)
+                    }
+                    error={
+                      formik.touched.pet?.race && Boolean(formik.errors.pet?.race)
+                    }
+                    helperText={formik.touched.pet?.race && formik.errors.pet?.race}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <FormControl
+                    fullWidth
+                    error={
+                      formik.touched.pet?.size && Boolean(formik.errors.pet?.size)
+                    }
+                  >
+                    <InputLabel id="mascota-tamano-label">
+                      Seleccioná Tamaño
+                    </InputLabel>
+                    <Select
+                      labelId="mascota-tamano-label"
+                      id="size"
+                      name="size"
+                      value={formik.values.pet.size}
+                      onChange={(event) =>
+                        formik.setFieldValue("pet.size", event.target.value)
+                      }
+                    >
+                      <MenuItem value="tipo1">pequeño</MenuItem>
+                      <MenuItem value="tipo2">mediano</MenuItem>
+                      <MenuItem value="tipo3">grande</MenuItem>
+                    </Select>
+                    <FormHelperText>
+                      {formik.touched.pet?.size && formik.errors.pet?.size}
+                    </FormHelperText>
+                  </FormControl>
+                </Grid>
+              </Grid>
+            </Stack>
           </Grid>
+
           <Button
             color="primary"
             variant="contained"
@@ -324,13 +330,13 @@ const Register: NextPage = () => {
               href="#"
               underline="hover"
               onClick={handleRegisterLinkClick}
-            >              
+            >
               <span style={{ color: "#007FFF" }}> Inicia sesión</span>
             </Link>
           </Box>
         </form>
       </Paper>
-    </Box>
+    </Box >
   );
 };
 
