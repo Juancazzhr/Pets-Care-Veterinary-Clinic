ecf5dd61f5f274487e7431a970177008b7a1cf01
Juan Camilo Hernández Ramírez <Juancazzhr@gmail.com>
2023-11-17 20:05:26 -0500

    refactor(back-end): enable cross origin in ms-gateway

--- back-end/ms-gateway/src/main/java/com/petscare/msgateway/MsGatewayApplication.java
+++ back-end/ms-gateway/src/main/java/com/petscare/msgateway/MsGatewayApplication.java
@@ -3,11 +3,9 @@ package com.petscare.msgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@CrossOrigin(origins = "*")
 public class MsGatewayApplication {
 
     public static void main(String[] args) {
