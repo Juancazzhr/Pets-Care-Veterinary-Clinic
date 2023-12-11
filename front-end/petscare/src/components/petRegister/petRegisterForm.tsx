6ed996931faee2d6aa861826e2c4a63c7b8ae672
Marina Victoria Blanco <marivic_05@hotmail.com>
2023-12-11 20:48:48 -0300

    Arreglo css registro mascota

--- front-end/petscare/src/components/petRegister/petRegisterForm.tsx
+++ front-end/petscare/src/components/petRegister/petRegisterForm.tsx
@@ -3,7 +3,7 @@ import { FC, useCallback, useEffect, useState } from 'react';
 import { yupResolver } from '@hookform/resolvers/yup'
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Box, Button, FormHelperText, Grid, MenuItem, Select, SelectChangeEvent, Stack, Typography } from '@mui/material';
+import { Box, Button, FormControl, FormHelperText, Grid, InputLabel, MenuItem, Select, SelectChangeEvent, Stack, Typography } from '@mui/material';
 import { getUserByEmail } from "../../services/userService";
 import { postPet } from "../../services/petService";
 import InputController from "../userRegister/inputController";
@@ -15,14 +15,14 @@ import { useRouter } from 'next/router';
 
 
 const initialValues = {
-    name: "",
-    size: "",
-    clientId: 0,
-    petType: {
-      id: 0,
-      typeName: "",
-    },
-    race: ""
+  name: "",
+  size: "",
+  clientId: 0,
+  petType: {
+    id: 0,
+    typeName: "",
+  },
+  race: ""
 }
 
 const PetRegisterForm: FC = () => {
@@ -44,48 +44,48 @@ const PetRegisterForm: FC = () => {
     acceptButtonText: "",
   });
 
-  
+
   const handleModalClose = useCallback(() => {
     setIsModalOpen(false);
-   }, []);
-    
- const redirectToPetRegistration = useCallback(() => {
-   setIsModalOpen(false);
-   router.push("/client");
+  }, []);
+
+  const redirectToPetRegistration = useCallback(() => {
+    setIsModalOpen(false);
+    router.push("/client");
   }, []);
 
   const onSubmit: SubmitHandler<any> = (data: any) => {
     console.log(data);
 
-    const dataUser = ()=>{
+    const dataUser = () => {
       if (user?.email === undefined) {
         // Manejar el caso en que no hay email
-    } else {
+      } else {
         const dataUser = getUserByEmail(user.email);
         console.log(dataUser);
+      }
     }
-    }
-     /*  getUserByEmail(user?.email)*/
-    console.log(dataUser); 
+    /*  getUserByEmail(user?.email)*/
+    console.log(dataUser);
 
     let tipo = 0;
-    switch ( petType) {
-      case 'Felino' :
-      tipo = 1
+    switch (petType) {
+      case 'Felino':
+        tipo = 1
         break;
-      case 'Canino' :
-      tipo = 2
+      case 'Canino':
+        tipo = 2
         break;
-      case 'Aves' :
-      tipo = 3
+      case 'Aves':
+        tipo = 3
         break;
       case 'Equino':
-      tipo = 4
-        break;    
+        tipo = 4
+        break;
       default:
         break;
     }
-    
+
     setDataForm({
       ...dataForm,
       name: data.name,
@@ -104,13 +104,13 @@ const PetRegisterForm: FC = () => {
       size: size,
       clientId: 0,
       petType: {
-        id:tipo,
+        id: tipo,
         typeName: petType
       },
       race: data.race,
     }
 
-        
+
     const response = postPet(dataPet)
     response.then((res) => {
       if (res.ok) {
@@ -125,74 +125,66 @@ const PetRegisterForm: FC = () => {
     })
   }
 
-  // useEffect(() => {
-  //   if (user) {
-  //     setDataForm({ ...dataForm, email: user.email })
-  //   }
-  // }, [user])
 
   console.log({ user });
   console.log({ dataForm });
 
   return (
     <>
-      <Grid item xs={12} xl={12}>
-        <Typography color="primary" variant="h6">
-          Mascota 1
-        </Typography>
-      </Grid>
       <Stack className={styles.bgBoxMascota}>
-        <Grid container spacing={2} >
-          <form onSubmit={handleSubmit(onSubmit)}>
-          <Grid item xs={12} xl={6} >
-            <InputController
-              name='name'
-              label='Nombre'
-              type='text'
-              defaultValue={dataForm.name}
-              disabled={false}
-              control={control}
-              error={errors.name ? true : false}
-              message={errors.name?.message as string} />
-          </Grid>
-          <Grid item xs={12} xl={6}>
-            <InputController
-              name='race'
-              label='Raza'
-              type='text'
-              defaultValue={dataForm.race}
-              disabled={false}
-              control={control}
-              error={errors.race ? true : false}
-              message={errors.race?.message as string} />
-          </Grid>
-          <Grid item xs={12} xl={6} >
-            <label className={styles.label}>Tamaño</label>
-            <Select  
+        <Typography color="primary" variant="h6">
+          Mascota
+        </Typography>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <InputController
+            name='name'
+            label='Nombre'
+            type='text'
+            defaultValue={dataForm.name}
+            disabled={false}
+            control={control}
+            error={errors.name ? true : false}
+            message={errors.name?.message as string} />
+
+          <InputController
+            name='race'
+            label='Raza'
+            type='text'
+            defaultValue={dataForm.race}
+            disabled={false}
+            control={control}
+            error={errors.race ? true : false}
+            message={errors.race?.message as string} />
+
+          <FormControl className={styles.formControl} >
+            <InputLabel id="mascota-tipo-label">Tamaño</InputLabel>
+            <Select
               className={styles.inputPet}
               labelId="mascota-tamano-label"
               id="size"
               name="size"
               value={size}
-              onChange={(event) => setSize( event.target.value)}
-              >
-                <MenuItem  value="pequeño">pequeño</MenuItem>
-                <MenuItem value="mediano">mediano</MenuItem>
-                <MenuItem value="grande">grande</MenuItem>
-              </Select>
-              {/* <FormHelperText className={styles.help}>
+              onChange={(event) => setSize(event.target.value)}
+            >
+              <MenuItem value="pequeño">pequeño</MenuItem>
+              <MenuItem value="mediano">mediano</MenuItem>
+              <MenuItem value="grande">grande</MenuItem>
+            </Select>
+            {/* <FormHelperText className={styles.help}>
               {errors.size?.message as string}
               </FormHelperText> */}
-          </Grid>
-          <Grid item xs={12} xl={6}>
-          <label  className={styles.label} >Tipo</label>
+          </FormControl>
+
+          <FormControl className={styles.formControl} >
+            <InputLabel id="mascota-tipo-label">Tipo</InputLabel>
             <Select
               className={styles.inputPet}
               labelId="mascota-tipo-label"
               id="petType"
               name="petType"
               value={petType}
-              onChange={(event) => setpetType( event.target.value)} >
+              onChange={(event) => setpetType(event.target.value)} >
               <MenuItem value="Felino">Felino</MenuItem>
               <MenuItem value="Canino">Canino</MenuItem>
               <MenuItem value="Aves">Aves</MenuItem>
@@ -201,7 +193,8 @@ const PetRegisterForm: FC = () => {
             {/* <FormHelperText className={styles.help}>
               {errors.petType?.message as string}              
               </FormHelperText> */}
-          </Grid>
+          </FormControl>
+
           <Button
             color="primary"
             variant="contained"
@@ -210,16 +203,15 @@ const PetRegisterForm: FC = () => {
           >
             registrar
           </Button>
-            <ReusableModal
-              isOpen={isModalOpen}
-              onClose={handleModalClose}
-              onAccept={redirectToPetRegistration}
-              title={modalInfo.title}
-              message={modalInfo.message}
-              acceptButtonText={modalInfo.acceptButtonText}
-            />
-          </form>
-        </Grid>
+          <ReusableModal
+            isOpen={isModalOpen}
+            onClose={handleModalClose}
+            onAccept={redirectToPetRegistration}
+            title={modalInfo.title}
+            message={modalInfo.message}
+            acceptButtonText={modalInfo.acceptButtonText}
+          />
+        </form>
       </Stack>
     </>
   )
