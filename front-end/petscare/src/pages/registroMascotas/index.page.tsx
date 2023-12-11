031e2bedb98c6b04b7da8642f8620dee1e241926
Marina Victoria Blanco <marivic_05@hotmail.com>
2023-12-11 20:52:45 -0300

    Borro código innecesario page registroMascotas

--- front-end/petscare/src/pages/registroMascotas/index.page.tsx
+++ front-end/petscare/src/pages/registroMascotas/index.page.tsx
@@ -1,18 +1,13 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 import { NextPage } from "next";
 import LayoutAuth from "../../components/layouts/LayoutAuth";
 import styles from "../../components/userRegister/registro.module.css";
 import { Box, Grid, Typography, Paper } from "@mui/material";
 import PetRegisterForm from "../../components/petRegister/petRegisterForm";
-import { Pet } from "@/interfaces";
 
-
-interface Props {  
-  pet: Pet
-}
 
 const RegisterPets: NextPage = () => {
-  
+
   return (
     <Box className={styles.root}>
       <Typography
@@ -22,18 +17,10 @@ const RegisterPets: NextPage = () => {
         fontWeight={700}
         sx={{ mb: "15px" }}
       >
-        registra tu mascota
+        registrá tu mascota
       </Typography>
       <Paper elevation={8} className={styles.paper}>
-          <Grid container spacing={2}>
-            <PetRegisterForm  />
-          </Grid>
-          {/* <Box className={styles.addPets}>
-            <AddCircleIcon color="primary" />
-            <Typography color="primary" ml="10px" variant="body1">
-              AGREGAR MASCOTA
-            </Typography>
-          </Box> */}                    
+        <PetRegisterForm />
       </Paper>
     </Box>
   );
